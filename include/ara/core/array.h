561d6c0d982abbd99c80ed81ea5a890e484da6ac
Pawel Zima <pawel.zima@umlaut.com>
2020-06-10 21:36:26 +0200

    Implementation of ara::core::Array finished

--- include/ara/core/array.h
+++ include/ara/core/array.h
@@ -11,7 +11,120 @@ namespace ara::core
      * @req {SWS_CORE_01201}
      */
     template <typename T, std::size_t N>
-    class Array : public std::array<T, N> {};
+    class Array : private std::array<T, N>
+    {
+        public:
+            /**
+             * @brief Access specified element with bounds checking.
+             * 
+             */
+            using std::array<T, N>::at;
+            
+            /**
+             * @brief Access specified element.
+             * 
+             */
+            using std::array<T, N>::operator[];
+
+            /**
+             * @brief Access the first element.
+             * 
+             */
+            using std::array<T, N>::front;
+
+            /**
+             * @brief Access the last element.
+             * 
+             */
+            using std::array<T, N>::back;
+
+            /**
+             * @brief Direct access to the underlying array.
+             * 
+             */
+            using std::array<T, N>::data;
+            
+            /**
+             * @brief Returns an iterator to the beginning.
+             * 
+             */
+            using std::array<T, N>::begin;
+
+            /**
+             * @brief Returns an iterator to the beginning.
+             * 
+             */
+            using std::array<T, N>::cbegin;
+
+            /**
+             * @brief Returns an iterator to the end.
+             * 
+             */
+            using std::array<T, N>::end;
+
+            /**
+             * @brief Returns an iterator to the end.
+             * 
+             */
+            using std::array<T, N>::cend;
+
+            /**
+             * @brief Returns a reverse iterator to the beginning.
+             * 
+             */
+            using std::array<T, N>::rbegin;
+
+            /**
+             * @brief Returns a reverse iterator to the beginning.
+             * 
+             */
+            using std::array<T, N>::crbegin;
+
+            /**
+             * @brief Returns a reverse iterator to the end.
+             * 
+             */
+            using std::array<T, N>::rend;
+
+            /**
+             * @brief Returns a reverse iterator to the end.
+             * 
+             */
+            using std::array<T, N>::crend;
+
+            /**
+             * @brief Checks whether the container is empty.
+             * 
+             */
+            using std::array<T, N>::empty;
+
+            /**
+             * @brief Returns the number of elements.
+             * 
+             */
+            using std::array<T, N>::size;
+
+            /**
+             * @brief Returns the maximum possible number of elements.
+             * 
+             */
+            using std::array<T, N>::max_size;
+
+            /**
+             * @brief Fill the container with specified value.
+             * 
+             */
+            using std::array<T, N>::fill;
+        
+            /**
+             * @brief Swaps the contents.
+             * 
+             */
+            template<typename U>
+            constexpr auto swap(U&& other) {
+                return std::array<T, N>::swap(std::forward<U>(other));
+            }
+    };
 
     /**
      * @brief Exchanges content between arrays.
--- src/executable2/main.cpp
+++ src/executable2/main.cpp
@@ -1,15 +1,58 @@
 #include <iostream>
 #include <ara/core/array.h>
 
+#define TEST_SZ 10
+
 int main()
 {
-    ara::core::Array<int, 10> test_array;
+    ara::core::Array<int, TEST_SZ> a1;
+    ara::core::Array<int, TEST_SZ> a2;
 
-    for(int i = 0; i < 10; i++)
-        test_array[i] = i;
-        
-    for(int i = 0; i < 10; i++)
-        std::cout << test_array[i] << std::endl;
+    std::cout << std::endl << "1/x Write data to arrays" << std::endl;
+    for(int i = 0; i < TEST_SZ; i++)
+    {
+        a1[i] = i;
+        a2[i] = i*i;
+    }
+
+    std::cout << std::endl << "2/x Reading data from arrays" << std::endl;
+    for(int i = 0; i < TEST_SZ; i++)
+        std::cout << a1[i] << " " << a2[i] << std::endl;
+
+    std::cout << std::endl << "3/x ara::core::swap" << std::endl;
+    ara::core::swap(a1, a2);
+
+    std::cout << std::endl << "4/x Reading data from arrays" << std::endl;
+    for(int i = 0; i < TEST_SZ; i++)
+        std::cout << a1[i] << " " << a2[i] << std::endl;
+
+    std::cout << std::endl << "5/x at" << std::endl;
+    std::cout << a1.at(9) << " " << a2.at(9) << std::endl;
+
+    std::cout << std::endl << "6/x front" << std::endl;
+    std::cout << a1.front() << " " << a2.front() << std::endl;
+
+    std::cout << std::endl << "7/x back" << std::endl;
+    std::cout << a1.back() << " " << a2.back() << std::endl;
+
+    std::cout << std::endl << "8/x data" << std::endl;
+    for(int i = 0; i < TEST_SZ; i++)
+        std::cout << a1.data()[i] << " " << a2.data()[i] << std::endl;
+
+    std::cout << std::endl << "9/x begin/cbegin end/cend" << std::endl;
+    std::for_each(a1.cbegin(), a1.cend(), [](int v)
+    {
+        std::cout << v << std::endl;
+    });
+
+    std::cout << std::endl << "10/x empty / size / max_size" << std::endl;
+    std::cout << a1.empty() << " " << a1.size() << " " << a1.max_size() << std::endl;
+
+    std::cout << std::endl << "11/x fill" << std::endl;
+    a1.fill(0xAB);
+    a2.fill(0xBA);
+    for(int i = 0; i < TEST_SZ; i++)
+        std::cout << a1[i] << " " << a2[i] << std::endl;
 
     return 0;
 }
