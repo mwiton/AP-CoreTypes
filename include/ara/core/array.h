542e428c96713c68e2e0e6436192a025524cd2b5
Pawel Zima <pawel.zima@umlaut.com>
2020-06-11 20:03:04 +0200

    implementation changed

--- include/ara/core/array.h
+++ include/ara/core/array.h
@@ -2,6 +2,7 @@
 #define ARRAY_H
 
 #include <array> // std::array
+#include <iterator>
 
 namespace ara::core
 {
@@ -11,126 +12,187 @@ namespace ara::core
      * @req {SWS_CORE_01201}
      */
     template <typename T, std::size_t N>
-    class Array : private std::array<T, N>
+    class Array
     {
         public:
+            Array() {}
+
             /**
              * @brief Access specified element with bounds checking.
              * 
+             * @param i position of thge element to return.
              */
-            using std::array<T, N>::at;
+            constexpr T& at(int i) { return d_.at(i); }
+
+            /**
+             * @brief Access specified element with bounds checking.
+             * 
+             * @param i position of thge element to return.
+             */
+            constexpr const T& at(int i) const { return d_.at(i); }
             
             /**
              * @brief Access specified element.
              * 
+             * @param i position of the element to return.
+             */ 
+            constexpr T& operator [] (int i) { return d_[i]; }
+            
+            /**
+             * @brief Access specified element.
+             * 
+             * @param i position of the element to return.
              */
-            using std::array<T, N>::operator[];
+            constexpr const T& operator [] (int i) const { return d_[i]; }
 
             /**
              * @brief Access the first element.
              * 
              */
-            using std::array<T, N>::front;
+            constexpr T& front() { return d_.front(); }
+
+            /**
+             * @brief Access the first element.
+             * 
+             */
+            constexpr const T& front() const { return d_.front(); }
 
             /**
              * @brief Access the last element.
              * 
              */
-            using std::array<T, N>::back;
+            constexpr T& back() { return d_.back(); }
 
             /**
-             * @brief Direct access to the underlying array.
+             * @brief Access the last element.
              * 
              */
-            using std::array<T, N>::data;
-            
+            constexpr const T& back() const { return d_.back(); }
+
+            /**
+             * @brief Direct access to underlying array.
+             * 
+             */
+            constexpr T* data() { return d_.data(); }
+
+            /**
+             * @brief Direct access to underlying array.
+             * 
+             */
+            constexpr const T* data() const { return d_.data(); }
+
             /**
              * @brief Returns an iterator to the beginning.
              * 
              */
-            using std::array<T, N>::begin;
+            constexpr std::array<T, N>::iterator begin() { return d_.begin(); }
 
             /**
              * @brief Returns an iterator to the beginning.
              * 
              */
-            using std::array<T, N>::cbegin;
+            constexpr std::array<T, N>::const_iterator begin() const { return d_.begin(); }
+
+            /**
+             * @brief Returns an iterator to the beginning.
+             * 
+             */
+            constexpr std::array<T, N>::const_iterator cbegin() const { return d_.cbegin(); }
 
             /**
              * @brief Returns an iterator to the end.
              * 
              */
-            using std::array<T, N>::end;
+            constexpr std::array<T, N>::iterator end() { return d_.end(); }
 
             /**
              * @brief Returns an iterator to the end.
              * 
              */
-            using std::array<T, N>::cend;
+            constexpr std::array<T, N>::const_iterator end() const { return d_.end(); }
+
+            /**
+             * @brief Returns an iterator to the end.
+             * 
+             */
+            constexpr std::array<T, N>::const_iterator cend() const { return d_.cend(); }
 
             /**
              * @brief Returns a reverse iterator to the beginning.
              * 
              */
-            using std::array<T, N>::rbegin;
+            constexpr std::array<T, N>::reverse_iterator rbegin() { return d_.rbegin(); }
 
             /**
              * @brief Returns a reverse iterator to the beginning.
              * 
              */
-            using std::array<T, N>::crbegin;
+            constexpr std::array<T, N>::const_reverse_iterator rbegin() const { return d_.rbegin(); }
+
+            /**
+             * @brief Returns a reverse iterator to the beginning.
+             * 
+             */
+            constexpr std::array<T, N>::const_reverse_iterator crbegin() const { return d_.crbegin(); }
 
             /**
              * @brief Returns a reverse iterator to the end.
              * 
              */
-            using std::array<T, N>::rend;
+            constexpr std::array<T, N>::reverse_iterator rend() { return d_.rend(); }
 
             /**
              * @brief Returns a reverse iterator to the end.
              * 
              */
-            using std::array<T, N>::crend;
+            constexpr std::array<T, N>::const_reverse_iterator rend() const { return d_.rend(); }
+
+            /**
+             * @brief Returns a reverse iterator to the end.
+             * 
+             */
+            constexpr std::array<T, N>::const_reverse_iterator crend() const { return d_.crend(); }
 
             /**
              * @brief Checks whether the container is empty.
              * 
              */
-            using std::array<T, N>::empty;
+            [[nodiscard]] constexpr bool empty() const { return d_.empty(); }
 
             /**
              * @brief Returns the number of elements.
              * 
              */
-            using std::array<T, N>::size;
+            constexpr unsigned int size() const { return d_.size(); }
 
             /**
              * @brief Returns the maximum possible number of elements.
              * 
              */
-            using std::array<T, N>::max_size;
+            constexpr unsigned int max_size() const { return d_.max_size(); }
 
             /**
              * @brief Fill the container with specified value.
              * 
+             * @param value the value to assign to the elements.
              */
-            using std::array<T, N>::fill;
-        
+            constexpr void fill(const T& value) { d_.fill(value); }
+
             /**
              * @brief Swaps the contents.
              * 
+             * @param other container to exchange the contents with.
              */
-            template<typename U>
-            constexpr auto swap(U&& other) {
-                return std::array<T, N>::swap(std::forward<U>(other));
-            }
+            constexpr void swap(Array<T,N>& other) { std::swap(d_, other.d_); }
+        private:
+            std::array<T, N> d_;
     };
 
     /**
      * @brief Exchanges content between arrays.
      * 
-     * @param lhs first argument of swap invocation
-     * @param rhs second argument of swap invocation
+     * @param lhs first argument of swap invocation.
+     * @param rhs second argument of swap invocation.
      * 
      * @req {SWS_CORE_01296}
      */
