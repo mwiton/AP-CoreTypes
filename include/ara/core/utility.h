a3ddfe36378289ba73eadc3b0df616af0b3040fc
Mateusz Witon <mwiton@gmail.com>
2020-06-20 20:30:38 +0200

    Added container functions

--- include/ara/core/utility.h
+++ include/ara/core/utility.h
@@ -1,3 +1,9 @@
+/**
+ * Copytight (c) 2020
+ * umlaut Software Development and contributors
+ * 
+ * SPDX-License-Identifier: MIT
+ */
 #ifndef ARA_CORE_UTILITY_H_
 #define ARA_CORE_UTILITY_H_
 
@@ -53,5 +59,51 @@ template<typename T> using in_place_type_t = std::in_place_type_t<T>;
  * @req {SWS_CORE_04031, SWS_CORE_04032}
  */
 template<std::size_t I> using in_place_index_t = std::in_place_index_t<I>;
+
+template<typename Container> constexpr auto data(Container& c)
+  -> decltype(c.data())
+{
+    return std::data(c);
+}
+
+template <typename Container> constexpr auto data (Container const &c) -> decltype(c.data())
+{
+    return std::data(c);
+}
+
+template <typename T, std::size_t N> constexpr T* data (T(&array)[N]) noexcept
+{
+    return std::data(array);
+}
+
+template <typename E> constexpr E const * data (std::initializer_list< E > il) noexcept
+{
+    return std::data(il);
+}
+
+template <typename Container> constexpr auto size (Container const &c) -> decltype(c.size())
+{
+    return std::size(c);
+}
+
+template <typename T, std::size_t N> constexpr std::size_t size (T(&array)[N]) noexcept
+{
+    return std::size(array);
+}
+
+template <typename Container> constexpr auto empty (Container const &c) -> decltype(c.empty())
+{
+    return std::empty(c);
+}
+
+template <typename T, std::size_t N> constexpr bool empty (T(&array)[N]) noexcept
+{
+    return std::empty(array);
+}
+
+template <typename E> constexpr bool empty (std::initializer_list< E > il) noexcept
+{
+    return std::empty(il);
+}
 }  // namespace ara::core
 #endif  // ARA_CORE_ERRORCODE_H_
--- tests/utility_test.cpp
+++ tests/utility_test.cpp
@@ -48,3 +48,62 @@ TEST_CASE("in_place_index_t can be constructed", "[SWS_CORE], [SWS_CORE_04032]")
 {
     REQUIRE_NOTHROW(ara::core::in_place_index_t<1u>{});
 }
+
+TEST_CASE("Returns pointer for data in container", "[SWS_CORE], [SWS_CORE_04110]")
+{
+    std::vector<int> container{1, 2, 3};
+    REQUIRE(ara::core::data(container) == std::data(container));
+}
+
+
+TEST_CASE("Returns pointer for data in const container", "[SWS_CORE], [SWS_CORE_04111]")
+{
+    const std::vector<int> container{1, 2, 3};
+    REQUIRE(ara::core::data(container) == std::data(container));
+}
+
+
+TEST_CASE("Returns pointer for data in array", "[SWS_CORE], [SWS_CORE_04112]")
+{
+    int array[]{1, 2, 3};
+    REQUIRE(ara::core::data(array) == std::data(array));
+}
+
+
+TEST_CASE("Returns pointer for data in initializer_list", "[SWS_CORE], [SWS_CORE_04113]")
+{
+    std::initializer_list<int> il_list{1, 2, 3};
+    REQUIRE(ara::core::data(il_list) == std::data(il_list));
+}
+
+TEST_CASE("Returns size of container", "[SWS_CORE], [SWS_CORE_04120]")
+{
+    std::vector<int> container{1, 2, 3};
+    REQUIRE(ara::core::size(container) == std::size(container));
+}
+
+
+TEST_CASE("Returns size of array", "[SWS_CORE], [SWS_CORE_04121]")
+{
+    int array[]{1, 2, 3};
+    REQUIRE(ara::core::size(array) == std::size(array));
+}
+
+TEST_CASE("Returns if container empty", "[SWS_CORE], [SWS_CORE_04130]")
+{
+    std::vector<int> container{1, 2, 3};
+    REQUIRE(ara::core::empty(container) == std::empty(container));
+}
+
+TEST_CASE("Returns if array empty", "[SWS_CORE], [SWS_CORE_04131]")
+{
+    int array[]{1, 2, 3};
+    REQUIRE(ara::core::empty(array) == std::empty(array));
+}
+
+
+TEST_CASE("Returns if initializer_list empty", "[SWS_CORE], [SWS_CORE_04132]")
+{
+    std::initializer_list<int> il_list{1, 2, 3};
+    REQUIRE(ara::core::empty(il_list) == std::empty(il_list));
+}
