8df8e205661a5def34fca70832efe3057829882f
Mateusz Witon <mwiton@gmail.com>
2020-06-20 13:29:38 +0200

    Added in place tags

--- include/ara/core/utility.h
+++ include/ara/core/utility.h
@@ -1,9 +1,57 @@
 #ifndef ARA_CORE_UTILITY_H_
 #define ARA_CORE_UTILITY_H_
 
-//#include <cstddef>
+#include <utility>
 
 namespace ara::core {
+/**
+ * Ara::core type alias for std::byte
+ *
+ * It is a type that is able to hold a “byte” of the machine.
+ * It is an owntype distinct from any other type.
+ *
+ * @req {SWS_CORE_04200}
+ */
 using Byte = std::byte;
-}
+
+/**
+ * Ara::core type alias for std::in_place_t
+ *
+ * An instance of this type can be passed to certain constructors of
+ * ara::core::Optional to denote the intention that construction of the
+ * contained type shall be done in-place.
+ *
+ * @req {SWS_CORE_04011, SWS_CORE_04012}
+ */
+using in_place_t = std::in_place_t;
+
+/**
+ * Instance of ara::core::in_place_t
+ *
+ * @req {SWS_CORE_04013}
+ */
+inline constexpr in_place_t in_place{};
+
+/**
+ * Ara::core type alias for in_place_type_t
+ *
+ * An instance of this type can be passed to certain constructors of
+ * ara::core::Variant to denote the intention that construction of the contained
+ * type shall be done in-place. Denote an type-distinguishing operation.
+ *
+ * @req {SWS_CORE_04021, SWS_CORE_04022}
+ */
+template<typename T> using in_place_type_t = std::in_place_type_t<T>;
+
+/**
+ * Ara::core type alias for std::in_place_index_t
+ *
+ * An instance of this type can be passed to certain constructors of
+ * ara::core::Variant to denote the intention that construction of the contained
+ * type shall be done in-place. Denote an index-distinguishing operation.
+ *
+ * @req {SWS_CORE_04031, SWS_CORE_04032}
+ */
+template<std::size_t I> using in_place_index_t = std::in_place_index_t<I>;
+}  // namespace ara::core
 #endif  // ARA_CORE_ERRORCODE_H_
--- tests/utility_test.cpp
+++ tests/utility_test.cpp
@@ -4,7 +4,47 @@
 
 #include "ara/core/utility.h"
 
-TEST_CASE("Byte is alias for std::byte", "[SWS_CORE], [SWS_CORE_04200]")
+TEST_CASE("Byte is an alias for std::byte", "[SWS_CORE], [SWS_CORE_04200]")
 {
     REQUIRE(std::is_same_v<ara::core::Byte, std::byte>);
+}
+
+TEST_CASE("ara::core::in_place_t is an alias for std::in_place_t",
+          "[SWS_CORE], [SWS_CORE_04011]")
+{
+    REQUIRE(std::is_same_v<ara::core::in_place_t, std::in_place_t>);
+}
+
+TEST_CASE("in_place_t can be constructed", "[SWS_CORE], [SWS_CORE_04012]")
+{
+    REQUIRE_NOTHROW(ara::core::in_place_t{});
+}
+
+TEST_CASE("There is instance of in_place_t", "[SWS_CORE], [SWS_CORE_04013]")
+{
+    REQUIRE_NOTHROW((void) ara::core::in_place);
+}
+
+TEST_CASE("ara::core::in_place_type_t is an alias for std::in_place_type_t",
+          "[SWS_CORE], [SWS_CORE_04021]")
+{
+    REQUIRE(std::is_same_v<ara::core::in_place_type_t<int>,
+                           std::in_place_type_t<int>>);
+}
+
+TEST_CASE("in_place_type_t can be constructed", "[SWS_CORE], [SWS_CORE_04022]")
+{
+    REQUIRE_NOTHROW(ara::core::in_place_type_t<int>{});
+}
+
+TEST_CASE("ara::core::in_place_index_t is an alias for std::in_place_index_t",
+          "[SWS_CORE], [SWS_CORE_04031]")
+{
+    REQUIRE(std::is_same_v<ara::core::in_place_index_t<1u>,
+                           std::in_place_index_t<1u>>);
+}
+
+TEST_CASE("in_place_index_t can be constructed", "[SWS_CORE], [SWS_CORE_04032]")
+{
+    REQUIRE_NOTHROW(ara::core::in_place_index_t<1u>{});
 }
