23b587797af03c7166aa68f07bb3f0cbda142074
Mateusz Witon <mwiton@gmail.com>
2020-06-21 13:46:29 +0200

    Updated tests

--- tests/utility_test.cpp
+++ tests/utility_test.cpp
@@ -49,14 +49,16 @@ TEST_CASE("in_place_index_t can be constructed", "[SWS_CORE], [SWS_CORE_04032]")
     REQUIRE_NOTHROW(ara::core::in_place_index_t<1u>{});
 }
 
-TEST_CASE("Returns pointer for data in container", "[SWS_CORE], [SWS_CORE_04110]")
+TEST_CASE("Returns pointer for data in container",
+          "[SWS_CORE], [SWS_CORE_04110]")
 {
     std::vector<int> container{1, 2, 3};
     REQUIRE(ara::core::data(container) == std::data(container));
 }
 
 
-TEST_CASE("Returns pointer for data in const container", "[SWS_CORE], [SWS_CORE_04111]")
+TEST_CASE("Returns pointer for data in const container",
+          "[SWS_CORE], [SWS_CORE_04111]")
 {
     const std::vector<int> container{1, 2, 3};
     REQUIRE(ara::core::data(container) == std::data(container));
@@ -70,7 +72,8 @@ TEST_CASE("Returns pointer for data in array", "[SWS_CORE], [SWS_CORE_04112]")
 }
 
 
-TEST_CASE("Returns pointer for data in initializer_list", "[SWS_CORE], [SWS_CORE_04113]")
+TEST_CASE("Returns pointer for data in initializer_list",
+          "[SWS_CORE], [SWS_CORE_04113]")
 {
     std::initializer_list<int> il_list{1, 2, 3};
     REQUIRE(ara::core::data(il_list) == std::data(il_list));
@@ -91,19 +94,37 @@ TEST_CASE("Returns size of array", "[SWS_CORE], [SWS_CORE_04121]")
 
 TEST_CASE("Returns if container empty", "[SWS_CORE], [SWS_CORE_04130]")
 {
-    std::vector<int> container{1, 2, 3};
-    REQUIRE(ara::core::empty(container) == std::empty(container));
+    WHEN("Container is empty")
+    {
+        std::vector<int> container{};
+        REQUIRE(ara::core::empty(container));
+    }
+    WHEN("Container is not empty")
+    {
+        std::vector<int> container{1, 2, 3};
+        REQUIRE_FALSE(ara::core::empty(container));
+    }
 }
 
 TEST_CASE("Returns if array empty", "[SWS_CORE], [SWS_CORE_04131]")
 {
-    int array[]{1, 2, 3};
-    REQUIRE(ara::core::empty(array) == std::empty(array));
+    WHEN("Array is not empty")
+    {
+        int array[]{1, 2, 3};
+        REQUIRE_FALSE(ara::core::empty(array));
+    }
+    // array can not be empty, so no true case for empty
 }
 
 
 TEST_CASE("Returns if initializer_list empty", "[SWS_CORE], [SWS_CORE_04132]")
 {
-    std::initializer_list<int> il_list{1, 2, 3};
-    REQUIRE(ara::core::empty(il_list) == std::empty(il_list));
+    WHEN("initializer_list is empty")
+    {
+        REQUIRE(ara::core::empty(std::initializer_list<int>{}));
+    }
+    WHEN("initializer_list is not empty")
+    {
+        REQUIRE_FALSE(ara::core::empty({1, 2, 3}));
+    }
 }
