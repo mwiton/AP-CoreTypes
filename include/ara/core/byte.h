5a738172dfd4a9185569fee4e30b77bb6fd0073f
Mateusz Witon <mwiton@gmail.com>
2020-07-05 22:30:23 +0200

    Added comments

--- include/ara/core/byte.h
+++ include/ara/core/byte.h
@@ -11,35 +11,84 @@
 #include <cstddef>
 
 namespace ara::core {
+/**
+ * @brief Wrapper class for std::byte class
+ *
+ */
 class ByteImpl
 {
  public:
     using ByteType = uint8_t;
 
+    /**
+     * @brief Construct a new Byte Impl object with no defined value
+     *
+     */
     ByteImpl() = default;
+    /**
+     * @brief Constuct object with given value
+     * @arg Value of byte
+     *
+     */
     constexpr explicit ByteImpl(ByteType value) : impl{value} {}
 
+    /**
+     * @brief Default destructor
+     *
+     */
     ~ByteImpl() = default;
 
+    /**
+     * @brief Cast operator for machine byte type
+     *
+     * @return ByteType Value of byte given in byte machine type
+     */
     constexpr explicit operator ByteType() const
     {
         return static_cast<ByteType>(impl);
     }
 
+    /**
+     * @brief Equality operator for Byte object
+     *
+     * @return true When given Byte object is equal
+     * @return false When given Byte object is not equal
+     */
     constexpr bool operator==(const ByteImpl&) const = default;
 
+    /**
+     * @brief No equality operator for Byte object
+     *
+     * @return true When given Byte object is not equal
+     * @return false When given Byte object is equal
+     */
     constexpr bool operator!=(const ByteImpl&) const = default;
 
  private:
     std::byte impl;
 };
 
+/**
+ * @brief Gives value of Byte object as integer.
+ *
+ * @tparam IntegerType Exact type of integer to return
+ * @param b Byte object
+ * @return constexpr IntegerType Integer value
+ */
 template<class IntegerType> constexpr IntegerType
 to_integer(ByteImpl b) noexcept requires std::integral<IntegerType>
 {
     return IntegerType(static_cast<ByteImpl::ByteType>(b));
 }
 
+/**
+ * @brief Left shift operator for Byte object
+ *
+ * @tparam IntegerType Type of integer used by argument
+ * @param b Byte object
+ * @param shift Amount bits to shift
+ * @return constexpr ByteImpl Object with shifted value
+ */
 template<class IntegerType> constexpr ByteImpl
 operator<<(ByteImpl b, IntegerType shift) noexcept
   requires std::integral<IntegerType>
@@ -47,6 +96,14 @@ operator<<(ByteImpl b, IntegerType shift) noexcept
     return ByteImpl(static_cast<ByteImpl::ByteType>(b) << shift);
 }
 
+/**
+ * @brief Left shift operator with assigment for Byte object
+ *
+ * @tparam IntegerType Type of integer used by argument
+ * @param b Byte object
+ * @param shift Amount bits to shift
+ * @return constexpr ByteImpl& Reference to object with shifted value
+ */
 template<class IntegerType> constexpr ByteImpl&
 operator<<=(ByteImpl& b, IntegerType shift) noexcept
   requires std::integral<IntegerType>
@@ -54,6 +111,14 @@ operator<<=(ByteImpl& b, IntegerType shift) noexcept
     return b = b << shift;
 }
 
+/**
+ * @brief Right shift operator for Byte object
+ *
+ * @tparam IntegerType Type of integer used by argument
+ * @param b Byte object
+ * @param shift Amount bits to shift
+ * @return constexpr ByteImpl Object with shifted value
+ */
 template<class IntegerType> constexpr ByteImpl
 operator>>(ByteImpl b, IntegerType shift) noexcept
   requires std::integral<IntegerType>
@@ -61,6 +126,14 @@ operator>>(ByteImpl b, IntegerType shift) noexcept
     return ByteImpl(static_cast<ByteImpl::ByteType>(b) >> shift);
 }
 
+/**
+ * @brief Right shift operator with assigment for Byte object
+ *
+ * @tparam IntegerType Type of integer used by argument
+ * @param b Byte object
+ * @param shift Amount bits to shift
+ * @return constexpr ByteImpl& Reference to object with shifted value
+ */
 template<class IntegerType> constexpr ByteImpl&
 operator>>=(ByteImpl& b, IntegerType shift) noexcept
   requires std::integral<IntegerType>
@@ -68,45 +141,92 @@ operator>>=(ByteImpl& b, IntegerType shift) noexcept
     return b = b >> shift;
 }
 
+/**
+ * @brief Bitwise OR operator for Byte object
+ *
+ * @param l Left byte object
+ * @param r Right byte object
+ * @return constexpr ByteImpl Object with result of OR operation
+ */
 constexpr ByteImpl operator|(ByteImpl l, ByteImpl r) noexcept
 {
     return ByteImpl(static_cast<ByteImpl::ByteType>(l)
                     | static_cast<ByteImpl::ByteType>(r));
 }
 
+/**
+ * @brief Bitwise OR operator with assigment for Byte object
+ *
+ * @param l Left byte object
+ * @param r Right byte object
+ * @return constexpr ByteImpl& Reference to object with result of OR operation
+ */
 constexpr ByteImpl& operator|=(ByteImpl& l, ByteImpl r) noexcept
 {
     return l = l | r;
 }
 
+/**
+ * @brief Bitwise AND operator for Byte object
+ *
+ * @param l Left byte object
+ * @param r Right byte object
+ * @return constexpr ByteImpl Object with result of AND operation
+ */
 constexpr ByteImpl operator&(ByteImpl l, ByteImpl r) noexcept
 {
     return ByteImpl(static_cast<ByteImpl::ByteType>(l)
                     & static_cast<ByteImpl::ByteType>(r));
 }
 
+/**
+ * @brief Bitwise AND operator with assigment for Byte object
+ *
+ * @param l Left byte object
+ * @param r Right byte object
+ * @return constexpr ByteImpl& Reference to object with result of AND operation
+ */
 constexpr ByteImpl& operator&=(ByteImpl& l, ByteImpl r) noexcept
 {
     return l = l & r;
 }
 
+/**
+ * @brief Bitwise XOR operator for Byte object
+ *
+ * @param l Left byte object
+ * @param r Right byte object
+ * @return constexpr ByteImpl Object with result of XOR operation
+ */
 constexpr ByteImpl operator^(ByteImpl l, ByteImpl r) noexcept
 {
     return ByteImpl(static_cast<ByteImpl::ByteType>(l)
                     ^ static_cast<ByteImpl::ByteType>(r));
 }
 
+/**
+ * @brief Bitwise XOR operator with assigment for Byte object
+ *
+ * @param l Left byte object
+ * @param r Right byte object
+ * @return constexpr ByteImpl& Reference to object with result of XOR operation
+ */
 constexpr ByteImpl& operator^=(ByteImpl& l, ByteImpl r) noexcept
 {
     return l = l ^ r;
 }
 
+/**
+ * @brief Bit inversion oprator for Byte object
+ *
+ * @param b Byte object
+ * @return constexpr ByteImpl Object with value after inverting bits
+ */
 constexpr ByteImpl operator~(ByteImpl b) noexcept
 {
     return ByteImpl(~static_cast<ByteImpl::ByteType>(b));
 }
 
-using Byte = ByteImpl;
 }  // namespace ara::core
 
 #endif  // ARA_CORE_BYTE_H_
--- tests/byte_test.cpp
+++ tests/byte_test.cpp
@@ -4,119 +4,120 @@
 
 #include "ara/core/byte.h"
 
-TEST_CASE("Byte is not integral type", "[SWS_CORE_10100]")
+TEST_CASE("ByteImpl is not integral type", "[SWS_CORE_10100]")
 {
-    REQUIRE(std::is_integral_v<ara::core::Byte> == 0);
+    REQUIRE(std::is_integral_v<ara::core::ByteImpl> == 0);
 }
 
-TEST_CASE("Size of Byte is one", "[SWS_CORE_10101]")
+TEST_CASE("Size of ByteImpl is one", "[SWS_CORE_10101]")
 {
-    REQUIRE(sizeof(ara::core::Byte) == 1);
+    REQUIRE(sizeof(ara::core::ByteImpl) == 1);
 }
 
-TEST_CASE("Byte should be constrained in unsigned char limits",
+TEST_CASE("ByteImpl should be constrained in unsigned char limits",
           "[SWS_CORE_10102]")
 {
-    REQUIRE_NOTHROW(ara::core::Byte{0});
-    REQUIRE_NOTHROW(ara::core::Byte{std::numeric_limits<unsigned char>::max()});
+    REQUIRE_NOTHROW(ara::core::ByteImpl{0});
+    REQUIRE_NOTHROW(
+      ara::core::ByteImpl{std::numeric_limits<unsigned char>::max()});
 }
 
-TEST_CASE("Byte can be created with integral type", "[SWS_CORE_10103]")
+TEST_CASE("ByteImpl can be created with integral type", "[SWS_CORE_10103]")
 {
-    REQUIRE_NOTHROW(ara::core::Byte{1});
-    constexpr ara::core::Byte byte{2};
+    REQUIRE_NOTHROW(ara::core::ByteImpl{1});
+    constexpr ara::core::ByteImpl byte{2};
     REQUIRE_NOTHROW((void) byte);
 }
 
-TEST_CASE("Byte can be default constructed", "[SWS_CORE_10104]")
+TEST_CASE("ByteImpl can be default constructed", "[SWS_CORE_10104]")
 {
-    REQUIRE_NOTHROW(ara::core::Byte{});
+    REQUIRE_NOTHROW(ara::core::ByteImpl{});
 }
 
-TEST_CASE("Byte is trivially destructable", "[SWS_CORE_10105]")
+TEST_CASE("ByteImpl is trivially destructable", "[SWS_CORE_10105]")
 {
-    REQUIRE(std::is_trivially_destructible_v<ara::core::Byte>);
+    REQUIRE(std::is_trivially_destructible_v<ara::core::ByteImpl>);
 }
 
-TEST_CASE("Byte is not implicitly convertible from any other type",
+TEST_CASE("ByteImpl is not implicitly convertible from any other type",
           "[SWS_CORE_10106]")
 {
-    REQUIRE_FALSE(std::is_convertible_v<uint8_t, ara::core::Byte>);
-    REQUIRE_FALSE(std::is_convertible_v<int, ara::core::Byte>);
-    REQUIRE_FALSE(std::is_convertible_v<char, ara::core::Byte>);
+    REQUIRE_FALSE(std::is_convertible_v<uint8_t, ara::core::ByteImpl>);
+    REQUIRE_FALSE(std::is_convertible_v<int, ara::core::ByteImpl>);
+    REQUIRE_FALSE(std::is_convertible_v<char, ara::core::ByteImpl>);
 }
 
-TEST_CASE("Byte is not implicitly convertible to any other type",
+TEST_CASE("ByteImpl is not implicitly convertible to any other type",
           "[SWS_CORE_10106]")
 {
-    REQUIRE_FALSE(std::is_convertible_v<ara::core::Byte, uint8_t>);
-    REQUIRE_FALSE(std::is_convertible_v<ara::core::Byte, int>);
-    REQUIRE_FALSE(std::is_convertible_v<ara::core::Byte, char>);
+    REQUIRE_FALSE(std::is_convertible_v<ara::core::ByteImpl, uint8_t>);
+    REQUIRE_FALSE(std::is_convertible_v<ara::core::ByteImpl, int>);
+    REQUIRE_FALSE(std::is_convertible_v<ara::core::ByteImpl, char>);
 }
 
-TEST_CASE("Byte can be converted to unsigned char", "[SWS_CORE_10106]")
+TEST_CASE("ByteImpl can be converted to unsigned char", "[SWS_CORE_10106]")
 {
     WHEN("Not in constant expression")
     {
         const unsigned char value{7};
-        ara::core::Byte     byteObject{value};
+        ara::core::ByteImpl byteObject{value};
         auto                byteValue = static_cast<unsigned char>(byteObject);
         REQUIRE(value == byteValue);
     }
 
     WHEN("In constant expression")
     {
-        const unsigned char       value{7};
-        constexpr ara::core::Byte byteObject{value};
+        const unsigned char           value{7};
+        constexpr ara::core::ByteImpl byteObject{value};
         constexpr auto byteValue = static_cast<unsigned char>(byteObject);
         REQUIRE(value == byteValue);
     }
 }
 
-TEST_CASE("Byte shall be comparable for equality", "[SWS_CORE_10106]")
+TEST_CASE("ByteImpl shall be comparable for equality", "[SWS_CORE_10106]")
 {
-    WHEN("Byte objects are equal")
+    WHEN("ByteImpl objects are equal")
     {
-        ara::core::Byte byteObject1{1}, byteObject2{1};
+        ara::core::ByteImpl byteObject1{1}, byteObject2{1};
         REQUIRE(byteObject1 == byteObject2);
     }
 
-    WHEN("Byte objects are not equal")
+    WHEN("ByteImpl objects are not equal")
     {
-        ara::core::Byte byteObject1{1}, byteObject2{2};
+        ara::core::ByteImpl byteObject1{1}, byteObject2{2};
         REQUIRE_FALSE(byteObject1 == byteObject2);
     }
 }
 
-TEST_CASE("Byte shall be comparable for non-equality", "[SWS_CORE_10106]")
+TEST_CASE("ByteImpl shall be comparable for non-equality", "[SWS_CORE_10106]")
 {
-    WHEN("Byte objects are equal")
+    WHEN("ByteImpl objects are equal")
     {
-        ara::core::Byte byteObject1{1}, byteObject2{1};
+        ara::core::ByteImpl byteObject1{1}, byteObject2{1};
         REQUIRE_FALSE(byteObject1 != byteObject2);
     }
 
-    WHEN("Byte objects are not equal")
+    WHEN("ByteImpl objects are not equal")
     {
-        ara::core::Byte byteObject1{1}, byteObject2{2};
+        ara::core::ByteImpl byteObject1{1}, byteObject2{2};
         REQUIRE(byteObject1 != byteObject2);
     }
 }
 
-TEST_CASE("Byte can be changed to Integral type", "[]")
+TEST_CASE("ByteImpl can be changed to Integral type", "[]")
 {
-    ara::core::Byte byteObject{1};
-    auto            int32Value(ara::core::to_integer<int32_t>(byteObject));
+    ara::core::ByteImpl byteObject{1};
+    auto                int32Value(ara::core::to_integer<int32_t>(byteObject));
     REQUIRE(int32Value == 1);
     auto uint16Value(ara::core::to_integer<uint16_t>(byteObject));
     REQUIRE(uint16Value == 1u);
 }
 
-TEST_CASE("Byte can be used with shift operators", "[]")
+TEST_CASE("ByteImpl can be used with shift operators", "[]")
 {
-    ara::core::Byte byteObject1{4}, byteObject2{1};
-    WHEN("Used operator >>") { REQUIRE(byteObject1 >> 2 == byteObject2); }
-    WHEN("Used operator <<") { REQUIRE(byteObject2 << 2 == byteObject1); }
+    ara::core::ByteImpl byteObject1{4}, byteObject2{1};
+    WHEN("Used operator >>") { REQUIRE((byteObject1 >> 2) == byteObject2); }
+    WHEN("Used operator <<") { REQUIRE((byteObject2 << 2) == byteObject1); }
     WHEN("Used operator >>=")
     {
         byteObject1 >>= 2;
@@ -129,15 +130,15 @@ TEST_CASE("Byte can be used with shift operators", "[]")
     }
 }
 
-TEST_CASE("Byte can be used with bitwise operators", "[]")
+TEST_CASE("ByteImpl can be used with bitwise operators", "[]")
 {
-    ara::core::Byte byteObject1{1}, byteObject2{2}, byteObject3{3};
+    ara::core::ByteImpl byteObject1{1}, byteObject2{2}, byteObject3{3};
     WHEN("Used |,&,^,~")
     {
-        REQUIRE((byteObject1 | byteObject3) == byteObject3);  // 01 | 11 = 11
-        REQUIRE((byteObject1 & byteObject3) == byteObject1);  // 01 & 11 = 01
-        REQUIRE((byteObject1 ^ byteObject3) == byteObject2);  // 01 ^ 11 = 10
-        REQUIRE((~ara::core::Byte{0xFE}) == byteObject1);     // ~0xFE = 0x01
+        REQUIRE((byteObject1 | byteObject3) == byteObject3);   // 01 | 11 = 11
+        REQUIRE((byteObject1 & byteObject3) == byteObject1);   // 01 & 11 = 01
+        REQUIRE((byteObject1 ^ byteObject3) == byteObject2);   // 01 ^ 11 = 10
+        REQUIRE((~ara::core::ByteImpl{0xFE}) == byteObject1);  // ~0xFE = 0x01
     }
     WHEN("Used |=")
     {
